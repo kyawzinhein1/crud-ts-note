18a7179903179a469c858cb1c3dfd1f8ff9d6527
Kyaw Zin Hein <kyawzinhein162@gmail.com>
2025-05-04 10:21:53 +0630

    logout

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -50,3 +50,14 @@ export const loginUser = asyncHandler(async (req: Request, res: Response) => {
     throw new Error("Invalid credentials.");
   }
 });
+
+export const logoutUser = asyncHandler(async (req: Request, res: Response) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({
+    message: "User logout.",
+  });
+});
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controller/user";
+import { loginUser, logoutUser, registerUser } from "../controller/user";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 export default router;
